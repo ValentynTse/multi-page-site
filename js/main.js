e726ba4c426a8f7bef50e04283b14d2949b71846
Valentyn <124665175+ValentynTse@users.noreply.github.com>
2023-03-21 14:43:24 +0200

    completes adaptation of all pages

--- js/main.js
+++ js/main.js
@@ -27,5 +27,5 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu__list');
 
 menuBtn.addEventListener('click', () => {
-menu.classList.toggle('list--active');
+  menu.classList.toggle('list--active');
 });
