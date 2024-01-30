1a1f4475abf25de20a37424bd191248770245710
ValentynTse <tsehichko@gmail.com>
2024-01-30 09:53:45 +0200

    adds_barba_cdn

--- js/main.js
+++ js/main.js
@@ -1,31 +1,68 @@
-const tabItem = document.querySelectorAll('.tabs__btn-item');
-const tabContent = document.querySelectorAll('.tabs__content-item');
+barba.init({
+  views: [{
+    namespace: 'home',
 
-tabItem.forEach(function (element) {
-  element.addEventListener('click', open);
+    afterEnter() {
+      homePage()
+    }
+  }],
+  transitions: [{
+    name: 'opacity-transition',
+    sync: true,
+    leave(data) {
+      return gsap.to(data.current.container, {
+        opacity: 0
+      });
+    },
+    enter(data) {
+      return gsap.from(data.next.container, {
+        opacity: 0
+      });
+    }
+  }]
 })
 
-function open(evt) {
-  const tabTarget = evt.currentTarget;
-  const button = tabTarget.dataset.button;
-  tabItem.forEach(function (item) {
-    item.classList.remove('tabs__btn-item--active');
+function homePage() {
+
+  const tabItem = document.querySelectorAll('.tabs__btn-item');
+  const tabContent = document.querySelectorAll('.tabs__content-item');
+
+  tabItem.forEach(function (element) {
+    element.addEventListener('click', open);
+  })
+
+  function open(evt) {
+    const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
+    tabItem.forEach(function (item) {
+      item.classList.remove('tabs__btn-item--active');
+    });
+
+    tabTarget.classList.add('tabs__btn-item--active');
+
+    tabContent.forEach(function (item) {
+      item.classList.remove('tabs__content-item--active')
+    });
+
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+
+  }
+
+  const menuBtn = document.querySelector('.menu-btn');
+  const menu = document.querySelector('.menu__list');
+
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('list--active');
   });
 
-  tabTarget.classList.add('tabs__btn-item--active');
-
-  tabContent.forEach(function (item) {
-    item.classList.remove('tabs__content-item--active')
+  const swiper = new Swiper(".swiper", {
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
   });
-
-  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
-
 }
-
-
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu__list');
-
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('list--active');
-});
